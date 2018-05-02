c08b783f34776c2b94ed1ae690af2f99d4832c8d
Masaya Takeda <844196@gmail.com>
2018-05-02 11:09:28 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'App'
+})
+</script>
---
+++ src/router/routes.ts
@@ -0,0 +1,11 @@
+import { RouteConfig } from 'vue-router'
+import Home from '@/views/Home.vue'
+
+const routes: RouteConfig[] = [
+  {
+    path: '/',
+    component: Home
+  }
+]
+
+export default routes
